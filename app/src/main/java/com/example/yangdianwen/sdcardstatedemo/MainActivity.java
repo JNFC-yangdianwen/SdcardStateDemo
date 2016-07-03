dfdd2b5be139c4866e2e8c2ae2924b6f5c1de6f4
yangdianwen <2550286303@qq.com>
2016-07-03 16:40:59 +0800

    使用动态注册了监听sd卡的状态

--- app/src/main/java/com/example/yangdianwen/sdcardstatedemo/MainActivity.java
+++ app/src/main/java/com/example/yangdianwen/sdcardstatedemo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.yangdianwen.sdcardstatedemo;
 
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SdcardRecevier sdcardRecevier;
+    private IntentFilter filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sdcardRecevier = new SdcardRecevier();
+        filter = new IntentFilter();
+        filter.addAction("android.intent.action.MEDIA_MOUNTED");
+        filter.addAction("android.intent.action.MEDIA_UNMOUNTED");
+        filter.addDataScheme("file");
+        findViewById(R.id.btn_regist).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                registerReceiver(sdcardRecevier, filter);
+            }
+        });
+        findViewById(R.id.btn_unregist).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unregisterReceiver(sdcardRecevier);
+            }
+        });
+
     }
 }
